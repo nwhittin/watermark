cf41e6b378dfab52e2a4a680673cc566ea4b1617
Nathan Whittington <nwhittin@gmail.com>
2022-12-05 16:09:28 -0500

    changed functionality of buttons and entries to account for user error

--- main.py
+++ main.py
@@ -29,14 +29,18 @@ class Img:
     def watermark_image(self):
         draw = ImageDraw.Draw(self.image)
         draw.text((0, 0), WATERMARK)
+        preview_button.config(state="normal")
+        save_button.config(state="normal")
 
     def preview_image(self):
         self.image.show()
 
     def update_entry(self):
         browse_entry.config(state="normal")
+        browse_entry.delete(0, tk.END)
         browse_entry.insert(0, self.filename_display)
         browse_entry.config(state="disabled")
+        new_entry.delete(0, tk.END)
         new_entry.insert(0, self.new_filename)
 
     def save_image(self):
@@ -45,6 +49,8 @@ class Img:
         browse_entry.delete(0, tk.END)
         browse_entry.config(state="disabled")
         new_entry.delete(0, tk.END)
+        preview_button.config(state="disabled")
+        save_button.config(state="disabled")
 
 
 def preview_image():
@@ -69,9 +75,9 @@ new_label = tk.Label(text="New filename")
 new_label.grid(row=1, column=0)
 new_entry = tk.Entry(width=20)
 new_entry.grid(row=1, column=1)
-preview_button = tk.Button(width=20, text="Preview", command=preview_image)
+preview_button = tk.Button(width=20, text="Preview", state="disabled", command=preview_image)
 preview_button.grid(row=1, column=2)
-save_button = tk.Button(width=20, text="Save", command=save_image)
+save_button = tk.Button(width=20, text="Save", state="disabled", command=save_image)
 save_button.grid(row=2, column=1)
 
 window.mainloop()
